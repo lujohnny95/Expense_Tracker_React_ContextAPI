9e3937d77a3dfc3579f38e362ac9e252627348ab
lujohnny95 <lu_johnny95@yahoo.com>
2022-02-08 20:00:09 +0000

    GlobalProvider, context, implemented

---
+++ src/context/GlobalState.js
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+//initial state:
+const initialState = {
+    transactions: [
+        { id: 1, text: "Gym membership", amount: -20 },
+        { id: 2, text: "Phone contract", amount: -30 },
+        { id: 3, text: "Salary", amount: 1500 },
+        { id: 4, text: "Car finance", amount: -350 },
+        { id: 5, text: "Rent", amount: -550 },
+    ]
+}
+
+//create context:
+export const GlobalContext = createContext(initialState);
+
+//provider context:
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return ( <GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider> );
+}
